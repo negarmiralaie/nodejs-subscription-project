943170eff7d48148913158df64fb8bb0124eef50
negar miralaie <miralaienegar@gmail.com>
2023-02-13 05:37:46 +0330

    feat -> created register

--- routers/v1/index.js
+++ routers/v1/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
+router.use('/auth', require('./auth'));
 router.use('/subscription', require('./subscription'));
 
 module.exports = router;
