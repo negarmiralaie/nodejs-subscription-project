c721ad160c0db8818727b7600bc69c86606d9c26
negar miralaie <miralaienegar@gmail.com>
2023-02-14 07:11:33 +0330

    feat->created list subscriptions

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -7,9 +7,7 @@ const { Op } = require('sequelize');
 const base = db.Subscription;
 const subscriptionController = {
   create: async (req, res) => {
-    console.log('dsa')
     try {
-      console.log('reched here')
       const schema = joi.object().keys({ 
         name: joi.string().required(),
         price: joi.number().required(),
@@ -19,18 +17,7 @@ const subscriptionController = {
       const { error, value } = schema.validate(req.body, { abortEarly: true });
       if (error) return response.validation(res, error);
 
-      // ! CAN USER MAKE MORE THAN 1 SUBSCRIPTION?
-      // const subscription = await db.Subscription.findByPk(value.examined_id);
-      // if (!subscription) {
-      //   return response.customError(
-      //     res,
-      //     res.t('CRUD.Not_Found', { name: res.t('joi.field.personnel') }),
-      //     404
-      //   );
-      // }
-
       const customer = req.customer;
-      console.log('customersd', customer)
       value.customer_id = customer.id;
 
       const subscription = await base.create(value);
@@ -41,6 +28,63 @@ const subscriptionController = {
       return response.catchError(res, error);
     }
   },
+
+  list: async (req, res) => {
+    try {
+      // base.truncate({cascade: true, restartIdentity:true}) //*
+      const Schema = joi.object().keys({
+        page: joi.number(),
+        limit: joi.number(),
+        filter: joi
+          .object()
+          .keys({
+            where: joi
+            .object()
+            .keys({
+              name: joi.string(),
+              price: joi.number(),
+              isActive: joi.boolean(),
+            })
+            .default({}),
+            order: joi
+              .object()
+              .keys({
+                by: joi.string().default('createdAt'),
+                sort: joi.string().only().allow('asc', 'desc').default('desc'),
+              })
+              .default({ by: 'createdAt', sort: 'desc' }),
+          })
+          .default({ order: { by: 'createdAt', sort: 'desc' } }),
+      });
+
+      const { error, value } = Schema.validate(req.query, { abortEarly: true });
+      if (error) return response.validation(res, error);
+
+      let where = {};
+      if (value.filter.where) {
+        where = {
+          ...value.filter.where,
+        };
+        if (where.name) where.name = where.name;
+        if (where.price) where.mobile = where.price;
+        if (where.isActive) where.status = where.isActive;
+      }
+
+      const subscriptions = await Helper.paginate(
+        base,
+        value.page,
+        {
+          order: [[value.filter.order.by, value.filter.order.sort]],
+          where,
+        },
+        value.limit
+      );
+
+      return response.success(res, subscriptions, res.t('CRUD.Success'));
+    } catch (error) {
+      return response.catchError(res, error);
+    }  },
+
 };
 
 module.exports = subscriptionController;
