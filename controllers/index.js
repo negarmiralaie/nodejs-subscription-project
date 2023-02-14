cd8bdd123d30717881441e564f07d5e4a5dc7011
negar miralaie <miralaienegar@gmail.com>
2023-02-14 08:07:08 +0330

    feat -> created list invoices

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
 module.exports = {
     subscriptionController: require('./subscriptionController'),
+    invoiceController: require('./invoiceController')
 };
--- routers/v1/index.js
+++ routers/v1/index.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use('/auth', require('./auth'));
 router.use('/subscription', require('./subscription'));
+router.use('/invoice', require('./invoice'));
 
 module.exports = router;
